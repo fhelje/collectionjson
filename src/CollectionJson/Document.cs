6a488d7dcd7618dc84e94dce1b03280561b232aa
Glenn Block <glenn.block@gmail.com>
2012-11-08 13:44:25 +0100

    refactored models into separate files

---
+++ src/CollectionJson/Document.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CollectionJson
+{
+    public class Document
+    {
+        public Collection Collection { get; set; }
+    }
+}
---
+++ src/CollectionJson/Template.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CollectionJson
+{
+    public class Template
+    {
+        public Template()
+        {
+            Data = new List<Data>();
+        }
+
+        public IList<Data> Data { get; private set; }
+    }
+}
